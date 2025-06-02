29c3fbccf061da317f1b87552161b946157a5587
Gaelle Timmermans <gaelle.timmermansr@interface3.be>
2025-06-02 11:53:34 +0200

    Add project files.

---
+++ Namespace/Personnage.cs
@@ -0,0 +1,16 @@
+﻿using Namespace.Inventaire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespace
+{
+
+    public class Personnage
+    {
+        public string name;
+        public Bag inventaire;
+    }
+}
---
+++ Namespace/Program.cs
@@ -0,0 +1,28 @@
+﻿//using Inventaire;
+//using Namespace;
+using Namespace.Inventaire;
+namespace Namespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Sans l'import de l'inventaire grace au using
+            //Inventaire.Item ObjetInventaire = new Inventaire.Item();
+            //ObjetInventaire.name = "Pépite";
+            //ObjetInventaire.value = 5000;
+
+            //Console.WriteLine($"Dans mon sac, j'ai une {ObjetInventaire.name} de {ObjetInventaire.value} Pokédollars !");
+
+            // Avec le using Inventaire
+            Item ObjetInventaire = new Item();
+            ObjetInventaire.name = "Pépite";
+            ObjetInventaire.value = 5000;
+
+            Console.WriteLine($"Dans mon sac, j'ai une {ObjetInventaire.name} de {ObjetInventaire.value} Pokédollars !");
+        }
+    }
+}
+
+
+
