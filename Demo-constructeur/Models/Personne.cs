912aa599908959a64fc73b432bb8d781b5ba3bb8
Gaelle Timmermans <gaelle.timmermansr@interface3.be>
2025-06-05 15:43:49 +0200

    Ajout demo constructeur

---
+++ Demo-constructeur/Models/Personne.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_constructeur.Models
+{
+    internal class Personne
+    {
+        private string _nom;
+
+        public string Nom
+        {
+            //lis la valeur et la retourne pour l'utilisation
+            get
+            {
+                // Méthode pour couper une chaine de caractère : substring(caractere de depart, longueur a prendre en compte) si longueur non précisée il prendra tout en compte.
+                return _nom.Substring(0, 1).ToUpper() + _nom.Substring(1).ToLower();
+            }
+            //sauvegarde la valeur et on y effectue les test
+            private set
+            {
+                value = value.Trim();
+                if (value.Length > 2)
+                {
+                    _nom = value;
+                }
+            }
+        }
+
+        //private string _prenom;
+
+        //public string Prenom
+        //{
+        //    //lis la valeur
+        //    get 
+        //    {
+        //        return _prenom;
+        //    }
+        //    //sauvegarde la valeur
+        //    set 
+        //    {
+        //        _prenom = value;
+        //    }
+        //}
+        public string Prenom { get; set; }
+
+
+        public string NomComplet
+        {
+            get
+            {
+                return $"{Prenom} {Nom}";
+            }
+        }
+
+        private DateOnly _dateNaissance;
+
+        public DateOnly DateNaissance
+        {
+            set
+            {
+                _dateNaissance = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Now.Year - _dateNaissance.Year;
+                if (DateTime.Now.Month < _dateNaissance.Month)
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        public Personne()
+        {
+            Nom = "Smith";
+            Prenom = "John";
+            DateNaissance = new DateOnly(1974, 01, 19);
+        }
+
+        public Personne(string paramNom) : this()
+        {
+            Nom = paramNom;
+        }
+
+        public Personne(string paramNom, string paramPrenom): this()
+        {
+            //Nom = paramNom;
+            Prenom = paramPrenom;
+        }
+        public Personne(string paramNom, string paramPrenom, DateOnly paramNaiss) 
+        {
+            Nom = paramNom;
+            Prenom = paramPrenom;
+            DateNaissance = paramNaiss;
+        }
+
+
+        public void JoyeuxAnniversaire()
+        {
+            if (DateTime.Now.Day == _dateNaissance.Day && DateTime.Now.Month == _dateNaissance.Month)
+            {
+                Console.WriteLine("Joyeux Anniversaire 🥳");
+            }
+            else
+            {
+                Console.WriteLine("Joyeux Non-Anniversaire 🥳");
+            }
+        }
+
+        
+    }
+}
---
+++ Demo-constructeur/Program.cs
@@ -0,0 +1,25 @@
+﻿using Demo_constructeur.Models;
+using System.Globalization;
+
+namespace Demo_constructeur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne quelconque = new Personne();
+
+            Console.WriteLine($"{quelconque.Nom} | {quelconque.Prenom} | {quelconque.Age}");
+
+            Personne deuxieme = new Personne("Alba", "Jessica");
+            Console.WriteLine($"{deuxieme.Nom} | {deuxieme.Prenom} | {deuxieme.Age}");
+
+            Personne nouvelle = new Personne("PtiteGoutte", "Justine");
+            Console.WriteLine($"{nouvelle.Nom} | {nouvelle.Prenom} | {nouvelle.Age}");
+
+            Personne troisieme = new Personne("Coucou", "Hibou", new DateOnly(1978, 03, 19));
+            Console.WriteLine($"{troisieme.Nom} | {troisieme.Prenom} | {troisieme.Age}");
+
+        }
+    }
+}
