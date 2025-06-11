e86ceda69c3d6181c348317effb1a4f6b9c51740
Gaelle Timmermans <gaelle.timmermansr@interface3.be>
2025-06-11 11:09:02 +0200

    exercice indexeur avec formateur

---
+++ Exercice-Monopolis/Models/Jeu.cs
@@ -0,0 +1,92 @@
+﻿using Exercice_Monopolis.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Monopolis.Models
+{
+    internal class Jeu
+    {
+        #region Variables membre et propriétés
+        private List<Joueur> _joueurs;
+        private List<CasePropriete> _plateau;
+
+        public Joueur[] Joueurs
+        {
+            get { return _joueurs?.ToArray(); }
+            /*private set
+            {
+                _joueurs = new List<Joueur>(value);
+            }*/
+        }
+
+        public CasePropriete[] Plateau
+        {
+            get
+            {
+                return _plateau.ToArray();
+            }
+        }
+        #endregion
+
+        #region Indexeurs
+        public CasePropriete this[int numeroCase]
+        {
+            get
+            {
+                numeroCase %= _plateau.Count;
+                return _plateau[numeroCase];
+            }
+        }
+
+        /// <summary>
+        /// Recherche d'un pion dans la liste de joueurs
+        /// </summary>
+        /// <param name="pion">le pion que l'on recherche</param>
+        /// <returns>Si un joueur existe avec le pion retourne le si non retourne null</returns>
+        public Joueur? this[Pions pion]
+        {
+            get
+            {
+                foreach (Joueur j in _joueurs)
+                {
+                    if (j.Pion == pion) return j;
+                }
+                return null;
+            }
+        }
+
+        #endregion
+
+        //public Jeu(List<CasePropriete> casesPlateau)
+        //{
+        //    _joueurs = new List<Joueur>();
+        //    _plateau = casesPlateau;
+
+        //}
+
+        public Jeu(CasePropriete[] casesPlateau)
+        {
+            _joueurs = new List<Joueur>();
+            _plateau = new List<CasePropriete>(casesPlateau);
+            // initialisation d'une liste à partir d'un array methode raccourcie
+            //_plateau = [.. casesPlateau];
+        }
+
+        public void AjouterJoueur(string nom, Pions pion)
+        {
+            foreach (Joueur j in _joueurs)
+            {
+                if (j.Pion == pion)
+                {
+                    Console.WriteLine($"Le pion {pion} est déjà pris.");
+                    return;
+                }
+            }
+            _joueurs.Add(new Joueur(nom, pion));
+        }
+
+    }
+}
--- Exercice-Monopolis/Program.cs
+++ Exercice-Monopolis/Program.cs
@@ -18,11 +18,32 @@ namespace Exercice_Monopolis
             //    Pion = Pions.DeACoudre
             //};
 
-            Joueur joueur = new Joueur("Luc", Pions.DeACoudre);
+            //Joueur joueur = new Joueur("Luc", Pions.DeACoudre);
             //{
-                //Nom = "Luc",
-                //Pion = Pions.DeACoudre
+            //Nom = "Luc",
+            //Pion = Pions.DeACoudre
             //};
+
+            Console.WriteLine("Joueur, veuillez indiquez votre nom :");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Veuillez choixir votre pion :");
+            string[] pionNames = Enum.GetNames<Pions>();
+
+            foreach (string pion in pionNames)
+            {
+                Console.WriteLine($"\t- {pion}");
+            }
+
+            Console.WriteLine("Quel pion choisissez-vous? ");
+            string userInput = Console.ReadLine();
+            Pions choice = Enum.Parse<Pions>(userInput);
+
+            Joueur joueur = new Joueur
+            (
+                userName,
+                choice
+            );
 
 
             Console.WriteLine($"Bonjour, je suis {joueur.Nom}, je joue avec le pion {joueur.Pion} et je me trouve en position {joueur.Position}.");
@@ -52,6 +73,30 @@ namespace Exercice_Monopolis
             //Joueur joueur1 = new Joueur();
             //CasePropriete maCase = new CasePropriete();
             //joueur1.Properietes = maCase;
+
+            //List<CasePropriete> cases = new List<CasePropriete>()
+            //{
+            //    new CasePropriete("Patio", Couleurs.BleuCiel, 20),
+            //    new CasePropriete("Accueil", Couleurs.BleuCiel, 23),
+            //    new CasePropriete("Bureau Sonia", Couleurs.Marron, 26),
+            //    new CasePropriete("Bureau Nicole", Couleurs.Marron, 26),
+            //    new CasePropriete("Bureau Laure", Couleurs.Marron, 30)
+            //};
+
+            CasePropriete[] cases =
+            {
+                new CasePropriete("Patio", Couleurs.BleuCiel, 20),
+                new CasePropriete("Accueil", Couleurs.BleuCiel, 23),
+                new CasePropriete("Bureau Sonia", Couleurs.Marron, 26),
+                new CasePropriete("Bureau Nicole", Couleurs.Marron, 26),
+                new CasePropriete("Bureau Laure", Couleurs.Marron, 30)
+            };
+
+            Jeu monopily = new Jeu(cases);
+            Console.WriteLine($"Votre plateau compte {monopily.Plateau.Length} cases.");
+
+            //cases.Add(new CasePropriete("WC Fille", Couleurs.Violet, 32));
+            Console.WriteLine($"Votre plateau compte {monopily.Plateau.Length} cases.");
 
         }
     }
